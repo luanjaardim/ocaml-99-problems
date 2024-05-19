884612ae2e20eaeaac0e3b91e7ed117501bd8c14
luanjaardim <luang.ajardim@gmail.com>
2024-05-19 18:42:57 -0300

    7-17 done.

--- problemsInOcaml/bin/main.ml
+++ problemsInOcaml/bin/main.ml
@@ -14,6 +14,50 @@ let main () =
   (* Problem 5 *)
   assert (Funcs.rev [ "a"; "b"; "c" ] = [ "c"; "b"; "a" ]);
   (* Problem 6 *)
-  assert (Funcs.is_palin [ "x"; "a"; "m"; "a"; "x" ] = true)
+  assert (Funcs.is_palin [ "x"; "a"; "m"; "a"; "x" ] = true);
+  (* Problem 7 *)
+  assert (
+    Funcs.flatten [ One "a"; Many [ One "b"; Many [ One "c"; One "d" ]; One "e" ] ]
+    = [ "a"; "b"; "c"; "d"; "e" ]);
+  (* Problem 8 *)
+  let longList = [ "a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e" ] in
+  assert (Funcs.compress longList = [ "a"; "b"; "c"; "a"; "d"; "e" ]);
+  (* Problem 9 *)
+  assert (
+    Funcs.pack longList
+    = [ [ "a"; "a"; "a"; "a" ]
+      ; [ "b" ]
+      ; [ "c"; "c" ]
+      ; [ "a"; "a" ]
+      ; [ "d" ]
+      ; [ "e"; "e"; "e"; "e" ]
+      ]);
+  (* Problem 10 *)
+  assert (Funcs.encode longList = [ 4, "a"; 1, "b"; 2, "c"; 2, "a"; 1, "d"; 4, "e" ]);
+  (* Problem 11 *)
+  assert (
+    Funcs.rle_encode longList
+    = [ Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d"; Many (4, "e") ]);
+  (* Problem 12 *)
+  assert (
+    Funcs.rle_decode
+      [ Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d"; Many (4, "e") ]
+    = longList);
+  (* Problem 13, same as 11 *)
+  (* Problem 14 *)
+  assert (
+    Funcs.dup_elems [ "a"; "b"; "c"; "c"; "d" ]
+    = [ "a"; "a"; "b"; "b"; "c"; "c"; "c"; "c"; "d"; "d" ]);
+  (* Problem 15 *)
+  assert (Funcs.replicate [ "a"; "b"; "c" ] 3 = [ "a"; "a"; "a"; "b"; "b"; "b"; "c"; "c"; "c" ]);
+  (* Problem 16 *)
+  assert (
+    Funcs.drop_nth [ "a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j" ] 3
+    = [ "a"; "b"; "d"; "e"; "g"; "h"; "j" ]);
+  (* Problem 17 *)
+  assert (
+    Funcs.split_at_pos [ "a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j" ] 3
+    = ([ "a"; "b"; "c" ], [ "d"; "e"; "f"; "g"; "h"; "i"; "j" ]));
+  assert (Funcs.split_at_pos [ "a"; "b"; "c"; "d" ] 5 = ([ "a"; "b"; "c"; "d" ], []))
 in
 main ()
--- problemsInOcaml/lib/funcs.ml
+++ problemsInOcaml/lib/funcs.ml
@@ -32,3 +32,91 @@ let rev list =
 ;;
 
 let is_palin list = list = rev list
+
+type 'a node =
+  | One of 'a
+  | Many of 'a node list
+
+let flatten (list : 'a node list) : 'a list =
+  let rec aux acc = function
+    | [] -> acc
+    | One h :: tl -> aux (h :: acc) tl
+    | Many l :: tl -> aux (aux acc l) tl
+  in
+  aux [] list |> List.rev
+;;
+
+let compress l =
+  let rec aux prev acc = function
+    | [] -> acc
+    | h :: tl -> if h = prev then aux prev acc tl else aux h (h :: acc) tl
+  in
+  try
+    let head = List.hd l in
+    aux head [ head ] (List.tl l) |> List.rev
+  with
+  | Failure _ -> []
+  | _ -> failwith "Something not expected happened"
+;;
+
+(* TODO: Not tail recursive, implement a tail recursive version *)
+let pack l =
+  let only_returns_elem elem _ = elem in
+  let rec aux elem cnt_same = function
+    | [] -> [ List.init cnt_same @@ only_returns_elem elem ]
+    | h :: tl ->
+      if h = elem
+      then aux elem (cnt_same + 1) tl
+      else List.init cnt_same (only_returns_elem elem) :: aux h 1 tl
+  in
+  try aux (List.hd l) 1 (List.tl l) with
+  | Failure _ -> []
+  | _ -> failwith "Something not expected happened"
+;;
+
+let encode l = List.map (fun x -> List.length x, List.hd x) @@ pack l
+
+type 'a rle =
+  | One of 'a
+  | Many of int * 'a
+
+let rle_encode l =
+  List.map (function
+    | 1, e -> One e
+    | n, e -> Many (n, e))
+  @@ encode l
+;;
+
+let rec rle_decode = function
+  | [] -> []
+  | One e :: tl -> e :: rle_decode tl
+  | Many (n, e) :: tl -> List.init n (fun _ -> e) @ rle_decode tl
+;;
+
+let dup_elems l = List.map (fun x -> [ x; x ]) l |> List.concat
+(* Another solution:
+   let rec dup_elems2 = function
+   | [] -> []
+   | h :: tl -> h :: h :: dup_elems2 tl
+   ;; *)
+
+(* This function could resolve the previous one too *)
+let replicate l qtd = List.map (fun x -> List.init qtd (fun _ -> x)) l |> List.concat
+
+let drop_nth l nth =
+  let rec aux acc cnt = function
+    | [] -> acc
+    | h :: tl -> if cnt = 0 then aux acc (nth - 1) tl else aux (h :: acc) (cnt - 1) tl
+  in
+  aux [] (nth - 1) l |> List.rev
+;;
+
+let split_at_pos l pos =
+  let rec aux cnt acc = function
+    | [] -> List.rev acc, []
+    | h :: tl as li -> if cnt = 0 then List.rev acc, li else aux (cnt - 1) (h :: acc) tl
+  in
+  aux pos [] l
+;;
+
+let sla = 1
